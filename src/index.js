9f96d709679c19febe51c118269837c6bc18e2f6
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-08-02 14:50:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Step 1. Import react-router functions
 import { BrowserRouter as Router, Route } from 'react-router-dom';
- 
+
 const Home = () => {
   return (
     <div>
@@ -10,7 +10,7 @@ const Home = () => {
     </div>
   );
 };
- 
+
 // Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render((
   <Router>
