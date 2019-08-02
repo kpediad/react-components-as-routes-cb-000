74ab542880126fe0015c3126d3059ad5841ad2c3
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-08-02 15:05:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -39,9 +39,11 @@ const Login = () => {
 // Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render((
   <Router>
+    <React.Fragment>
       <Route path="/" render={Home} />
       <Route exact path="/about" render={About} />
       <Route exact path="/login" render={Login} />
+    </React.Fragment>
   </Router>),
   document.getElementById('root')
 );
